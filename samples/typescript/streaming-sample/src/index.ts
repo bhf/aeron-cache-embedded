fb239d8d0d314f9550b00a741b1ebf288773a08d
bhf <sanjeev.sarda@gmail.com>
2026-01-17 11:58:36 +0000

    Fix Typescript streaming sample

--- samples/typescript/streaming-sample/src/index.ts
+++ samples/typescript/streaming-sample/src/index.ts
@@ -1,22 +1,46 @@
-import { EmbeddedAeronCache } from '@aeron-cache/embedded-client';
+import WebSocket from 'ws';
+
+// Polyfill global WebSocket for Node.js environment
+// @ts-ignore
+global.WebSocket = WebSocket;
+
+import {AeronCacheClient, EmbeddedAeronCache} from '@aeron-cache/embedded-client';
 
 async function main() {
-    const baseUrl = process.argv[2] || 'http://localhost:8080';
-    console.log(`Starting Streaming Sample against ${baseUrl}`);
+    const baseUrl = process.argv[2] || 'http://localhost:7070';
+    const wsUrl = process.argv[3] || 'http://localhost:7071';
 
-    const cache = new EmbeddedAeronCache(baseUrl);
+    console.log(`Starting Streaming Sample against ${baseUrl}, wsUrl: ${wsUrl}`);
+
+    const cacheClient = new AeronCacheClient(baseUrl, wsUrl);
+
     try {
-        await cache.connect();
+        await cacheClient.createCache('streaming-sample-cache');
+    } catch (e) {
+        console.error(e);
+    }
+
+    const cache = new EmbeddedAeronCache(cacheClient, 'streaming-sample-cache');
+    const onMessage = () => {
+        console.info('Got message from Aeron Cache')
+    };
+    const onError = () => {
+        console.error('Error in subscription');
+    };
+
+    cache.subscribe(onMessage, onError);
+    try {
         console.log("Connected. Waiting for updates on 'shared-key'...");
 
-        let lastValue = "";
-        
         // Loop forever checking for updates
-        setInterval(() => {
-            const current = cache.get('shared-key');
-            if (current !== undefined && current !== lastValue) {
-                console.log(`Observed change in 'shared-key': ${current}`);
-                lastValue = current;
+        setInterval(async () => {
+            try {
+                const current = await cache.getLocal('shared-key');
+                if (current !== undefined) {
+                    const result = JSON.stringify(current);
+                    console.log(`Read key 'shared-key': ${result}`);
+                }
+            } catch (e) {
             }
         }, 1000);
 
@@ -25,7 +49,6 @@ async function main() {
 
     } catch (err) {
         console.error(err);
-        cache.close();
     }
 }
 
